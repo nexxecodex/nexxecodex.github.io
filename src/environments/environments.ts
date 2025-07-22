9f3749cc910c9d3455c0c1dbee885ae503cba374
nexxecodex <nexxecodex@gmail.com>
2025-07-22 09:33:47 +0530

    a

---
+++ src/environments/environments.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: 'YOUR_API_KEY',
+      authDomain: 'your-app.firebaseapp.com',
+      projectId: 'your-project-id',
+      storageBucket: 'your-app.appspot.com',
+      messagingSenderId: 'SENDER_ID',
+      appId: 'YOUR_APP_ID'
+    }
+  };
+  
